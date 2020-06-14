0e4184d0a42ea225b793897562df8b5ef7a91311
Sebastien REMY <seb.remy@me.com>
2020-06-14 18:18:05 +0200

    initial commit

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -12,34 +12,7 @@ struct ContentView: View {
     
     
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-            
-            // more code to come
-            
-            struct User: Codable {
-                var name: String
-                var address: Address
-            }
-            
-            struct Address: Codable {
-                var street: String
-                var city: String
-            }
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
-            }
-        }   
+        Text("Hello") 
     }
 }
 
