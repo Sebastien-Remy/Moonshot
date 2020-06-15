d0a72f70463763fb53c7b4efced916e8ae6b716e
Sebastien REMY <seb.remy@me.com>
2020-06-15 08:56:14 +0200

    Organize project

---
+++ Moonshot/Views/MissionView.swift
@@ -0,0 +1,21 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Sebastien REMY on 15/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
